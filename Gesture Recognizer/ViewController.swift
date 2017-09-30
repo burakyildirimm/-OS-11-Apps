17bfd11e1dd5b51c3109c66a3d139e3b4305ba26
burak <34burki24@gmail.com>
2017-09-30 15:54:10 +0300

    Initial Commit

---
+++ Gesture Recognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gesture Recognizer
+//
+//  Created by burak on 30.09.2017.
+//  Copyright © 2017 Burak Yıldırım. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
